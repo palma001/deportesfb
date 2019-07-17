c853715118f33357d2eaadb4709ecf9151f6d69e
Luis Palma <palmaluis1997@gmail.com>
2019-07-17 11:42:28 -0400

    plantilla

--- src/App.vue
+++ src/App.vue
@@ -14,19 +14,19 @@
 
 <script>
 
-  export default {
-    name: 'App',
-    components: {
-      CoreCta: () => import('@/components/core/Cta'),
-      CoreDrawer: () => import('@/components/core/Drawer'),
-      CoreFooter: () => import('@/components/core/Footer'),
-      CoreToolbar: () => import('@/components/core/Toolbar'),
-      CoreView: () => import('@/components/core/View')
-    },
-    data () {
-      return {
-      //
-      }
+export default {
+  name: 'App',
+  components: {
+    CoreCta: () => import('@/components/core/Cta'),
+    CoreDrawer: () => import('@/components/core/Drawer'),
+    CoreFooter: () => import('@/components/core/Footer'),
+    CoreToolbar: () => import('@/components/core/Toolbar'),
+    CoreView: () => import('@/components/core/View')
+  },
+  data () {
+    return {
+    //
     }
   }
+}
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,15 +13,15 @@
 </template>
 
 <script>
-  export default {
-    name: 'Home',
+export default {
+  name: 'Home',
 
-    components: {
-      About: () => import('@/components/home/About'),
-      Articles: () => import('@/components/home/Articles'),
-      Banner: () => import('@/components/home/Banner'),
-      Social: () => import('@/components/home/Social'),
-      Subscribe: () => import('@/components/home/Subscribe')
-    }
+  components: {
+    About: () => import('@/components/home/About'),
+    Articles: () => import('@/components/home/Articles'),
+    Banner: () => import('@/components/home/Banner'),
+    Social: () => import('@/components/home/Social'),
+    Subscribe: () => import('@/components/home/Subscribe')
   }
+}
 </script>
