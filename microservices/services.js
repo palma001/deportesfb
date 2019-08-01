8718580721080e0678e9ac9227998d637efc8448
Luis Palma <palmaluis1997@gmail.com>
2019-08-01 08:49:14 -0400

    microservices

---
+++ microservices/services.js
@@ -0,0 +1,28 @@
+import axiosInstance, { updateAxiosInstance, setAxiosHeader } from './AxiosIntance'
+import config, { setConfig } from './config'
+export const getData = function (url, params) {
+  return axiosInstance.get(`${url[0]}/${url[1]}`, { params })
+    .then((res) => {
+      return { 'status': true, 'res': res }
+    })
+    .catch((err) => {
+      if (err) {
+        return { 'status': false, 'response': err }
+      }
+    })
+}
+
+const services = {
+  install (Vue, options = {}) {
+    setConfig(Object.assign(config, options))
+    updateAxiosInstance(options.baseUrl)
+    Vue.prototype.$services = {
+      getData,
+      updateAxiosInstance,
+      setAxiosHeader,
+      config
+    }
+  }
+}
+
+export default services
