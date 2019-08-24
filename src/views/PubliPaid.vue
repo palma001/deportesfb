6a0ebb8b786104f9a366272c6dc191991cc7d898
Luis Palma <palmaluis1997@gmail.com>
2019-08-24 08:39:25 -0430

    video

--- src/views/PubliPaid.vue
+++ src/views/PubliPaid.vue
@@ -11,6 +11,7 @@
 			<hr/>
 		</div>
 		<div class="parrafo">
+			<iframe width="100%" height="315" src="https://www.youtube.com/embed/OZxcJxejib0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 			<h4>
 				Registrate <a href="https://publipaid.com/r5OSE8">Aqui</a>
 				y mira los videos para que aprenadas a monetizar tu tiempo
