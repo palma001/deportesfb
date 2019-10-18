7506154cf77cdd12c9ac7f936fea553ff40d88ee
palma001 <palmaluis1997@gmail.com>
2019-10-17 22:29:50 -0400

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,5 @@
 <template>
   <div id="home">
-    <center>
-      <iframe data-aa="1231421" src="//ad.a-ads.com/1231421?size=728x90" scrolling="no" style="width:728px; height:90px; border:0px; padding:0; overflow:hidden" allowtransparency="true"></iframe>
-    </center>
     <articles>
       <banner />
     </articles>
@@ -12,9 +9,6 @@
     <subscribe />
 
     <social />
-    <center>
-      <a href="https://publipaid.com/r5OSE8"><img src="https://cdn.publipaid.com/static/banners/es/publipaid-g-468x60.gif"></a>
-    </center>
   </div>
 </template>
 
